1dc299b206cc382fa12678017e6c0c95821ee86f
Nikita <artimenko.nikita@gmail.com>
2019-10-29 20:32:15 +0000

    Frame switch added; Register frame switch added; Main buttons disapeared from the main window when compiling.

--- src/ru/artimenko/quizdemo/MainWindow.java
+++ src/ru/artimenko/quizdemo/MainWindow.java
@@ -9,7 +9,8 @@ public class MainWindow extends BaseFrame implements ActionListener {
     Panel teacherPanel = new Panel();
     Panel studentPanel = new Panel();
     Panel welcomePanel = new Panel();
-    Panel registerPanel = new Panel();
+    Panel registerPanelTeacher = new Panel();
+    Panel registerPanelStudent = new Panel();
     Button btnRegister = new Button("Register");
     Button btnTeacher = new Button("Teacher");
     Button btnStudent = new Button("Student");
@@ -30,21 +31,100 @@ public class MainWindow extends BaseFrame implements ActionListener {
         buttonPanel.add(btnRegister);
         buttonPanel.add(btnStudent);
         buttonPanel.add(btnTeacher);
+
         mainPanel.add(welcomePanel);
         this.add(buttonPanel, BorderLayout.NORTH);
         this.add(mainPanel, BorderLayout.CENTER);
-        registerPanel.add(new Label("I am Refister label"));
+
+        Panel pnlRegisterTeacher = new Panel();
+        Panel pnlInputTeacher = new Panel();
+        TextArea txtNameTeacher = new TextArea();
+        TextArea txtSurnameTeacher = new TextArea();
+        TextArea txtPasswordTeacher = new TextArea();
+        TextArea txtAccountName = new TextArea();
+        Button btnTeacherRegister = new Button("Teacher");
+        Button btnStudentRegister = new Button("Student");
+        Panel pnlRegister = new Panel();
+        Panel pnlRegisterStudent = new Panel();
+        Panel pnlInputStudent = new Panel();
+        TextArea txtNameStudent = new TextArea();
+        TextArea txtSurnameStudent = new TextArea();
+        TextArea txtPasswordStudent = new TextArea();
+        TextArea txtClassID = new TextArea();
+
+        registerPanelStudent.add(pnlRegisterStudent, BorderLayout.NORTH);
+        registerPanelStudent.add(pnlInputStudent, BorderLayout.CENTER);
+        registerPanelStudent.add(btnRegister, BorderLayout.SOUTH);
+
+        pnlRegister.add(btnStudent);
+        pnlRegister.add(btnTeacher);
+        pnlInputStudent.add(txtNameStudent);
+        pnlInputStudent.add(txtSurnameStudent);
+        pnlInputStudent.add(txtPasswordStudent);
+        pnlInputStudent.add(txtClassID);
+
+        pnlRegisterTeacher.add(btnStudentRegister);
+        pnlRegisterTeacher.add(btnTeacherRegister);
+        pnlInputTeacher.add(txtNameTeacher);
+        pnlInputTeacher.add(txtSurnameTeacher);
+        pnlInputTeacher.add(txtPasswordTeacher);
+        pnlInputTeacher.add(txtAccountName);
+        registerPanelTeacher.add(pnlRegisterTeacher, BorderLayout.NORTH);
+        registerPanelTeacher.add(btnRegister, BorderLayout.SOUTH);
+        registerPanelTeacher.add(pnlInputTeacher, BorderLayout.CENTER);
+
+        Panel pnlStuLogIn = new Panel();
+        TextArea txtStuAccountName = new TextArea();
+        TextArea txtStuPassword = new TextArea();
+        Button btnTeacherLogIn = new Button();
+
+        pnlStuLogIn.add(txtStuAccountName);
+        pnlStuLogIn.add(txtStuPassword);
+        studentPanel.add(pnlStuLogIn, BorderLayout.CENTER);
+        studentPanel.add(btnTeacherLogIn, BorderLayout.SOUTH);
+
+        Panel pnlTeachLogIn = new Panel();
+        Button btnTeachLogIn = new Button();
+        TextArea txtTeachAccountName = new TextArea();
+        TextArea txtTeachPassword = new TextArea();
+        TextArea txtTeachClassID = new TextArea();
+
+        pnlTeachLogIn.add(txtTeachAccountName);
+        pnlTeachLogIn.add(txtTeachClassID);
+        pnlTeachLogIn.add(txtTeachPassword);
+        teacherPanel.add(pnlTeachLogIn, BorderLayout.CENTER);
+        teacherPanel.add(btnTeachLogIn, BorderLayout.SOUTH);
     }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
-        if (e.getSource() == btnRegister) {
+        if (e.getSource() == btnStudent) {
             this.mainPanel.removeAll();
-            this.mainPanel.add(registerPanel);
+            this.mainPanel.add(studentPanel);
+        } else if (e.getSource() == btnTeacher) {
+            this.mainPanel.removeAll();
+            this.mainPanel.add(teacherPanel);
+        } else if (e.getSource() == btnRegister) {
+            Button btnTeacherRegister = new Button("Teacher");
+            Button btnStudentRegister = new Button("Student");
+            btnTeacherRegister.addActionListener(this);
+            btnStudentRegister.addActionListener(this);
+
+            Panel buttonPanelRegister = new Panel();
+            buttonPanelRegister.add(btnStudentRegister);
+            buttonPanelRegister.add(btnTeacherRegister);
+            buttonPanelRegister.setLayout(new GridLayout(1,2));
+            this.add(buttonPanelRegister, BorderLayout.NORTH);
+
+            if (e.getSource() == btnTeacherRegister) {
+                this.mainPanel.removeAll();
+                this.mainPanel.add(registerPanelTeacher);
+            } else {
+                this.mainPanel.removeAll();
+                this.mainPanel.add(registerPanelStudent);
+            }
         }
-//        repaint();
         this.mainPanel.repaint();
     }
 }
